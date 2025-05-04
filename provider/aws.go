8051c9294b1990082d493ccf5bed3c15041e85a3
root <root@vmi2384903.contaboserver.net>
2025-05-04 07:52:17 +0200

    mod

--- provider/aws.go
+++ provider/aws.go
@@ -15,8 +15,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/sa7mon/s3scanner/bucket"
-	"github.com/sa7mon/s3scanner/provider/clientmap"
+	"github.com/mux0x/S3Scanner/bucket"
+	"github.com/mux0x/S3Scanner/provider/clientmap"
 	log "github.com/sirupsen/logrus"
 )
 
